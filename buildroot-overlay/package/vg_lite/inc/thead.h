c2d20a2bd4b26dbbf1cb35acf302861c6ace9a6d
huangziyi <huangziyi@canaan-creative.com>
2024-07-19 12:36:20 +0800

    remove libthead.o, use th.dcache.civa

--- buildroot-overlay/package/vg_lite/inc/thead.h
+++ buildroot-overlay/package/vg_lite/inc/thead.h
@@ -7,7 +7,23 @@
 extern "C" {
 #endif
 
-void thead_csi_dcache_clean_invalid_range(void* addr, uint64_t size);
+static inline void thead_csi_dcache_clean_invalid_range(void* addr, uint64_t size) {
+    uint64_t op_addr = (uint64_t)addr;
+    int64_t op_size = size + op_addr % 64;
+    const int64_t linesize = 64;
+
+    __asm volatile("fence iorw, iorw");
+
+    while (op_size > 0) {
+        __asm volatile("th.dcache.civa %0" : : "r"(op_addr));
+        op_addr += linesize;
+        op_size -= linesize;
+    }
+
+    __asm volatile("fence iorw, iorw");
+    __asm volatile("fence.i");
+    __asm volatile("fence r, r");
+}
 
 #ifdef __cplusplus
 }
