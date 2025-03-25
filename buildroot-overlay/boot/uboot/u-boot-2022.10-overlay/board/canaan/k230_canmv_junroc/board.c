66b9c3a90691dfbf6257a395633dd549707662c4
jyh <jiangyuhan0316@outlook.com>
2025-03-25 20:21:27 +0800

    update

--- buildroot-overlay/boot/uboot/u-boot-2022.10-overlay/board/canaan/k230_canmv_junroc/board.c
+++ buildroot-overlay/boot/uboot/u-boot-2022.10-overlay/board/canaan/k230_canmv_junroc/board.c
@@ -22,6 +22,7 @@
 // #include "k230_board_common.h"
 #include <env_internal.h>
 #include <linux/delay.h>
+#include <dm.h>
 #include <platform.h>
 #include "../common/k230_board_common.h"
 
@@ -29,33 +30,13 @@
 
 sysctl_boot_mode_e sysctl_boot_get_boot_mode(void)
 {
-	return SYSCTL_BOOT_SDIO0;
+	return SYSCTL_BOOT_SDIO1;
 }
 
 #ifdef CONFIG_BOARD_LATE_INIT
 int board_late_init(void)
 {
-    #define USB_IDPULLUP0 		(1<<4)
-    #define USB_DMPULLDOWN0 	(1<<8)
-    #define USB_DPPULLDOWN0 	(1<<9)
-
-
-
-    u32 usb_ctl3 = readl((const volatile void __iomem *)(SDIO0_BASE_ADDR + 0x7c));
-
-	usb_ctl3 |= USB_IDPULLUP0;
-	usb_ctl3 &= ~(USB_DMPULLDOWN0 | USB_DPPULLDOWN0);
-	writel(usb_ctl3, ( volatile void __iomem *)(SDIO0_BASE_ADDR + 0x7c));
-	//printf("usb_ctl3 =%x\n",usb_ctl3);
-
-	usb_ctl3 = readl((const volatile void __iomem *)(SDIO0_BASE_ADDR + 0x9c));
-	usb_ctl3 |= USB_IDPULLUP0;
-	usb_ctl3 |= (USB_DMPULLDOWN0 | USB_DPPULLDOWN0);
-	writel(usb_ctl3, ( volatile void __iomem *)(SDIO0_BASE_ADDR + 0x9c));
-
-	env_set_ulong("mmc_boot_dev_num", g_bootmod - SYSCTL_BOOT_SDIO0);
-
-	//printf("usb_ctl3 =%x\n",usb_ctl3);
-    return 0;
+        env_set_ulong("mmc_boot_dev_num", g_bootmod - SYSCTL_BOOT_SDIO0);
+        return 0;
 }
 #endif
