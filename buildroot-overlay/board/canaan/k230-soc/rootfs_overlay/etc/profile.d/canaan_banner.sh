39ebe25d79e76e07323a292f4eb59ab0f29421db
wangjianxin <wangjianxin@canaan-creative.com>
2025-05-29 09:22:47 +0800

    add boot banner

---
+++ buildroot-overlay/board/canaan/k230-soc/rootfs_overlay/etc/profile.d/canaan_banner.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cat <<EOF
+
+$(echo -e "\033[1m")
+    Welcome to K230, for more information, please visit:$(echo -e "\033[0m")
+        https://www.kendryte.com/zh/document
+        https://www.kendryte.com/answer/
+        https://www.kendryte.com/zh/products
+        https://github.com/kendryte/k230_linux_sdk/
+        https://www.kendryte.com/k230_linux/dev/
+
+EOF
